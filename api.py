72fbefe04b948e5160c7798cfc9378af22044da1
Joseph Weller <joseph5062a@gmail.com>
2023-05-14 12:36:41 -0400

    prompt template & juypter notebook for ease of development

--- api.py
+++ api.py
@@ -4,33 +4,40 @@ from langchain import PromptTemplate
 from langchain.agents.tools import Tool
 from langchain.llms import OpenAIChat
 from langchain.utilities import GoogleSerperAPIWrapper
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 from termcolor import colored
 from dotenv import load_dotenv
+
 
 # Load environment variables from the .env file
 load_dotenv()
 
-article_link = "https://www.npr.org/2023/05/13/1176037743/biden-howard-university-2024" # TODO: make configurable
+article_link = "https://www.independent.co.uk/news/world/americas/us-politics/ron-desantis-daniel-penny-jordan-neely-b2338438.html" # TODO: make configurable
+
+response_schemas = [
+    ResponseSchema(name="1", description="first news article link"),
+    ResponseSchema(name="2", description="second news article link"),
+    ResponseSchema(name="3", description="third news article link"),
+]
+output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+format_instructions = output_parser.get_format_instructions()
 
 template = """
-You are an expert in detecting political bias.
-Use the search tool to find three separate news articles that have similar titles to this news article: {link}
-These three news articles will be called the perspective articles.
-The perspective articles must be written within the same relative time frame as the original news article.
-Perform a comparison of the content in the original article with the content in the perspective articles.
-Respond with a rating as to how politically biased the original news article is, using the perspective articles for reference.
-This rating should be from one to ten, one meaning that there's no political bias in the original article, and ten meaning that the original article is extremely politically biased.
+Use the search tool to find three separate news articles are written on the same subject of this news article: {link} 
+These articles must be written within the same relative time frame as the original news article.
+Respond with the link of each of the three articles.
+{format_instructions}
 """
 initial_prompt = PromptTemplate(
     input_variables=["link"],
-    template=template
+    template=template,
+    partial_variables={"format_instructions": format_instructions}
 )
 
 llm = OpenAIChat(model_name='gpt-3.5-turbo', temperature=0.0)
 
 serp_tool = GoogleSerperAPIWrapper(type="news")
-
-# TODO: Add Memory https://python.langchain.com/en/latest/modules/memory/how_to_guides.html
 
 tools = [
     Tool(name="Search Tool",
@@ -38,7 +45,7 @@ tools = [
         func=serp_tool.run)
 ]
 
-agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True, max_iterations=5)
 
-print(agent(initial_prompt.format(link=article_link)))
+related_article_links = output_parser.parse(agent(initial_prompt.format(link=article_link))["output"]).values()
 
