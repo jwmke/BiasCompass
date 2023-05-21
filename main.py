dd4c0cfe7a6b69a03a10c1126996fed1e2d969f8
Joseph Weller <joseph5062a@gmail.com>
2023-05-21 16:40:11 -0400

    the game plan

--- main.py
+++ main.py
@@ -60,4 +60,12 @@ response = oauth.post(
      json=payload
 )
 
+# TODO: 
+# 1. Test replying to a mention with the same text the user sends
+# 2. Get tweet id of all mentions
+# 3. Per mention: 
+#   1. Get article link (do some validation)
+#   2. Run article through compass_v2 logic
+#   3. Reply to tweet via id with evaluation
+
 print(response)
